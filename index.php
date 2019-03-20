4c2070c26c7ee1ee29ff6f1c9cf20cc1a07b540d
Mike Langone <langone@onlineimage.com>
2019-03-20 11:40:34 -0700

    change to owl

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
         </div>
     </div>
 
+
     <div class="grid-container">
       <div class="grid-x grid-padding-x align-center">
         <div class="small-12 medium-8 cell page-content-80">
