9f8cad981ffbc6312e0c7f96ae7e8f9d55b3713e
Mike Langone <langone@onlineimage.com>
2019-03-20 11:54:54 -0700

    change to owl

---
+++ salt.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Horizontal Buildings - Salt and Sand</title>
+	<link rel="stylesheet" href="css/app.css">
+	<link rel="stylesheet" href="js/owlcarousel/assets/owl.carousel.min.css">
+	<link rel="stylesheet" href="js/owlcarousel/assets/owl.theme.default.min.css">
+</head>
+<body>
+
+<?php include ('includes/header.php') ;?>
+
+<div class="hero-section-salt">
+	<div class="hero-section-text">
+	</div>
+</div>
+
+
+<div class="grid-container">
+	<div class="grid-x grid-padding-x align-center">
+		<div class="small-12 medium-10 cell page-content-80">
+			<div class="grid-x grid-padding-x grid-margin-x">
+				<div class="medium-4 cell">
+					<h2 class="section-title">Fabric Storage Structures for Salt & Sand</h2>
+					<div class="cta-graphic" style="padding-top: 2rem">
+						<img src="images/outline-button.png" alt="">
+					</div>
+				</div>
+				<div class="medium-8 cell">
+					<p>Britespan is the preferred choice for engineers, purchasing agents and operations management across North America. We build economical and long lasting bulk salt storage domes and sand storage sheds. </p>
+					<p>With a fabric building from Britespan, you can store and protect salt and sand from the elements, while reducing caking and runoff. Britespan fabric structures are engineered for operating machinery, allowing you to maximize cubic storage space and minimizing the overall cost per ton stored. </p>
+					<p>Our fabric buildings provide widths up to 300 feet and easily accommodate doors of all sizes. Our steel trusses are hot-dip galvanized after all fabrication is completed for superior corrosion resistance. Store more salt for less – flexible foundation options and building height provide the most cubic storage. </p>
+					<h6 class="grey-boldish" style="margin-bottom: 0; color: #767676">Britespan fabric buildings are the ideal choice for:</h6>
+						<p>Salt Storage Domes - The professionals from Britespan can install a durable, fixed storage dome as the main source for storing salt. It’s designed so that vehicles and equipment have room to go in and out without having interior columns that can cause interference. Sand Storage Sheds - Sand can be safely stored and kept dry under our domed, fabric structures. </p>
+					<p>Road Salt Storage - Our road salt storage buildings are optimal for storing road salt inconvenient, roadside locations. These can be temporary structures for seasonal use. </p>
+					<p>Split Storage - Make full use of Britespan’s fabric storage structures and make your storage multifunctional by installing a split storage building. </p>
+					<p>Palletized Salt Storage - Getting a fabric salt storage structure is ideal for palletized salt storage. Our dome-shaped structures are optimal for forklift use, creating a space that’s easy to navigate around.</p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<?php include ('includes/footer.php') ;?>
+
+<script src="js/jquery.js"></script>
+<script src="js/what-input.js"></script>
+<script src="js/foundation.js"></script>
+<script src="js/owlcarousel/owl.carousel.min.js"></script>
+<script src="js/app.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
